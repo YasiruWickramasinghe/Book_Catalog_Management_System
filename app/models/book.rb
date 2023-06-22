3243e2d9ad47bbc617da65188199f87b061b5080
YasiruWickramasinghe <it19038292@my.sliit.lk>
2023-06-22 23:26:24 +0530

    authentication completed with stylebook field validation complete

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,12 @@
 class Book < ApplicationRecord
     belongs_to :user
+
+    validates :title, presence: { message: "Title is required" }
+    validates :author, presence: { message: "Author is required" }
+    validates :genre, presence: { message: "Genre is required" }
+    validates :year_of_publication, presence: { message: "Year is required" }, numericality: {
+        greater_than_or_equal_to: 1900,
+        less_than_or_equal_to: Date.current.year,
+        message: "Year must be between 1900 and #{Date.current.year}"
+      }
 end
