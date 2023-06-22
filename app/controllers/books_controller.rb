0f4ca89bba3bcedf0ac46b72d41b0a463307c563
YasiruWickramasinghe <it19038292@my.sliit.lk>
2023-06-22 17:46:20 +0530

    authorization completed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,15 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   # GET /books or /books.json
   def index
-    @books = Book.all
+    if params[:search]
+      @books = Book.where("title LIKE ?", "%#{params[:search]}%")
+    else
+      @books = Book.all
+    end
   end
 
   # GET /books/1 or /books/1.json
@@ -12,7 +18,8 @@ class BooksController < ApplicationController
 
   # GET /books/new
   def new
-    @book = Book.new
+    #@book = Book.new
+    @book = current_user.books.build
   end
 
   # GET /books/1/edit
@@ -21,11 +28,12 @@ class BooksController < ApplicationController
 
   # POST /books or /books.json
   def create
-    @book = Book.new(book_params)
+    #@book = Book.new(book_params)
+    @book = current_user.books.build(book_params)
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to book_url(@book), notice: "Book was successfully created." }
+        format.html { redirect_to book_url(@book), notice: "Book Created Successfully!" }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +46,7 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to book_url(@book), notice: "Book was successfully updated." }
+        format.html { redirect_to book_url(@book), notice: "Book Updated Successfully!" }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,9 +60,14 @@ class BooksController < ApplicationController
     @book.destroy
 
     respond_to do |format|
-      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
+      format.html { redirect_to books_url, notice: "Book Deleted Successfully!" }
       format.json { head :no_content }
     end
+  end
+
+  def correct_user
+    @book = current_user.books.find_by(id: params[:id])
+    redirect_to books_path, notice: "Not Authorized To Edit This Book" if @book.nil?
   end
 
   private
@@ -64,7 +77,12 @@ class BooksController < ApplicationController
     end
 
     # Only allow a list of trusted parameters through.
+    #def book_params
+    #  params.require(:book).permit(:title, :author, :genre, :year_of_publication, :user_id)
+    #end
+
     def book_params
-      params.require(:book).permit(:title, :author, :genre, :year_of_publication)
+      params.require(:book).permit(:title, :author, :genre, :year_of_publication, :user_id, :search)
     end
+
 end
