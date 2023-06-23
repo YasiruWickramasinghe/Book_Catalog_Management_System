24fda140c73d85cae501e7269714332220c7494c
YasiruWickramasinghe <it19038292@my.sliit.lk>
2023-06-23 14:04:11 +0530

    Project Functions Completed

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,8 +5,8 @@ class Book < ApplicationRecord
     validates :author, presence: { message: "Author is required" }
     validates :genre, presence: { message: "Genre is required" }
     validates :year_of_publication, presence: { message: "Year is required" }, numericality: {
-        greater_than_or_equal_to: 1900,
+        greater_than_or_equal_to: 1600,
         less_than_or_equal_to: Date.current.year,
-        message: "Year must be between 1900 and #{Date.current.year}"
+        message: "Year must be between 1600 and #{Date.current.year}"
       }
 end
